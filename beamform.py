53c00b89b7ddc9dcd5ec71741de8c4257db17132
markrkrebs <markrkrebs@gmail.com>
2016-11-25 14:35:22 -0700

    Still very much WIP, some bugs fixed.
    Changed sign of delay addition to create an El angle squint: +y coords need lead (negative delay).
    Made a major correction in my misunderstanding of how to reduce a numpy matrix to an array that can be "viewed" through a flattened, single-dimension iterator.

--- beamform.py
+++ beamform.py
@@ -16,100 +16,28 @@ import numpy as np
 
 
 
-                         
-wavelength = 2.5;  #meters, wavelength we're working with
-                         
-class beam(object):
-    def __init__(self):
-        # here's the aperture AIP locatoins, in m.
-        # Here are some coordinates to start with, for a 7x7 array.
-        # Note the center element is at Xgrid=0, Ygrid=0.
-        # Later, I should pass these in, instead.
-        self.Ygrid =np.matrix([[-3.,-3.,-3.,-3.,-3.,-3.,-3.],
-                         [-2.,-2.,-2.,-2.,-2.,-2.,-2.],
-                         [-1.,-1.,-1.,-1.,-1.,-1.,-1.],
-                         [0.,0.,0.,0.,0.,0.,0.],
-                         [1.,1.,1.,1.,1.,1.,1.],
-                         [2.,2.,2.,2.,2.,2.,2.],
-                         [3.,3.,3.,3.,3.,3.,3.]]);
-        self.Xgrid= np.matrix([[-3.,-2.,-1.,0.,1.,2.,3.],
-                         [-3.,-2.,-1.,0.,1.,2.,3.],
-                         [-3.,-2.,-1.,0.,1.,2.,3.],
-                         [-3.,-2.,-1.,0.,1.,2.,3.],
-                         [-3.,-2.,-1.,0.,1.,2.,3.],
-                         [-3.,-2.,-1.,0.,1.,2.,3.],
-                         [-3.,-2.,-1.,0.,1.,2.,3.]]);
-        
-        self.gridshape = self.Ygrid.shape;                 
-        self.AzYgrid = np.zeros(self.gridshape);
-        self.AzXgrid = np.zeros(self.gridshape);  
-               
-        #This initial condition sends a beam out the boresight.  Remember, the
-        #zeros are the phase angles of the unit power vectors at each AIP.
-        self.BeamDelays= np.zeros(self.gridshape);
-        self.elements = self.Ygrid.size;  #number of elements
-        
-    def AzRotate(self,az):
-        # provides the x and y positions of the array AIPs, in
-        # coordinate frame rotated to align with the Az direction
-        # First, allocate space for the answer: that's a class data element
-        # so, as it's done, we can immediately load it.
-        cosAz=np.cos(az);
-        sinAz=np.sin(az);
-        # Sense of this rotation is CCW, with x right & y up, on the page
-        # So the rotation is +z, out of the page, towards you.
-        # Python question: can I assume the "self." eg on self.Xgrid?
-        Xarray=Xgrid.reshape(self.elements); #Danger: no copy. ObjRef.
-        Yarray=Ygrid.reshape(self.elements);
-        for i in range(0, self.elements) :
-            self.AzYgrid[i] = -Xarray[i]*sinAz + Yarray[i]*cosAz;
-            self.AzXgrid[i] = Xarray[i]*cosAz + Yarray[i]*sinAz;
-   
-    def RampAdd(el):
-        #Caution: you better have performed AzRotate first!
-        #We are going to load the BeamDelays, as needed to create
-        #the squint angle el, in the az frame defined by AzGrid.
-        #(el is the "squint" angle away from aperture boresight)
-        
-        c= 3e8;  #m/sec, speed of light
-        
-       #how much addtl delay must we add for every meter in -x
-       delayRamp = sin(el)/c;  #something like this
-    
-        #First, make a temp linear array pointer into BeamDelays
-        BeamDelayArray=self.BeamDelays.reshape(self.elements)
-        #Now go through the AIPs, adding delay proportionate to the x-coord
-        for i in range(0,self.elements):
-            BeamDelayArray[i]= delayRamp*AzYgrid[i];
-
-        
-    def aim(self,az,el):  #Loads the Delays for a commanded beam direction.
-        #Transform all the array AIP grid locations to the Az frame 
-        #Python Q: is self already local in AzRotate?
-        self.BeamAzAngle=az;  # make these part of the class state.
-        self.BeamElAngle=el;
-        self.AzRotate(az);  #x-form to az frame, where we can...
-        # apply the needed phase delays to all AIP to squint through el.
-        # (This function will set the BeamDelays)
-        self.RampAdd(el);
-                   
-
-#------------------------------------------------------------------
-# here's the Main.  Takes Az and El of the commanded beam direction, and 
-# arrays of the null direction commands.
-# N.B. This is not a class method.
-# N.B. fn definition syntax that puts defaults into the parameter list.
-
-def beamformExample(az=0.,el=30/57.3,Nulls=np.matrix([[0],[0]])):
+def beamFormExample(az=0.,el=30/57.3,Nulls=np.matrix([[0],[0]])):
    #compute the ramp delay in sec/meter, that will generate El
    B = beam();  # TODO I would like to pass in the AIP grid someday.
    
-   B.aim(az,el);  #This calculates the delays needed to aim the beam
+   B.aim(az,el);  #This calculates the delays needed to aim the beam.
    
   
    #Now that we know the delays, compute and plot the beam power for all Az-El
    #Also note a basic metric, the max ratio of all sidelobes to main beam pwr.
    #TODO: do that.
+  
+   #To calculate the ArrayFactor (an array itself, of  expressing the gain of 
+   #of the antenna array, at different angles) we need to define the domain
+  
+   #Somewhat arbitrarily, we'll make ArrayFactor the same shape.
+   ArrayFactor = np.zeros(B.shape); 
+   ArrayFactorAz = np.zeros(B.shape);
+   ArrayFactorEl = np.zeros(B.shape);
+   ArrayFactorArray=ArrayFactor.reshape(B.elements); #Danger: no copy. ObjRef.
+   for i in range(0,B.elements):
+       AzAFA[i]=B.AzAngle(i);
+       
   
   
 
@@ -157,8 +85,108 @@ def beamformExample(az=0.,el=30/57.3,Nulls=np.matrix([[0],[0]])):
    DelaySensitivity=calcDelaySensitivity(GeomDelays);
    
    
-   
+#-----------Supporting class-------------------------------------------
 
+                         
+wavelength = 2.5;  #meters, wavelength we're working with
+
+
+                         
+class beam(object):
+    def __init__(self):
+        # here's the aperture AIP locations, in m.
+        # Here are some coordinates to start with, for a 7x7 array.
+        # Note the center element is at Xgrid=0, Ygrid=0.
+        # Later, I should pass these in, instead.
+        self.Ygrid =np.matrix([[-3.,-3.,-3.,-3.,-3.,-3.,-3.],
+                         [-2.,-2.,-2.,-2.,-2.,-2.,-2.],
+                         [-1.,-1.,-1.,-1.,-1.,-1.,-1.],
+                         [0.,0.,0.,0.,0.,0.,0.],
+                         [1.,1.,1.,1.,1.,1.,1.],
+                         [2.,2.,2.,2.,2.,2.,2.],
+                         [3.,3.,3.,3.,3.,3.,3.]]);
+        self.Xgrid= np.matrix([[-3.,-2.,-1.,0.,1.,2.,3.],
+                         [-3.,-2.,-1.,0.,1.,2.,3.],
+                         [-3.,-2.,-1.,0.,1.,2.,3.],
+                         [-3.,-2.,-1.,0.,1.,2.,3.],
+                         [-3.,-2.,-1.,0.,1.,2.,3.],
+                         [-3.,-2.,-1.,0.,1.,2.,3.],
+                         [-3.,-2.,-1.,0.,1.,2.,3.]]);
+        
+        self.gridshape = self.Ygrid.shape;                 
+        self.AzYgrid = np.zeros(self.gridshape);
+        self.AzXgrid = np.zeros(self.gridshape);  
+               
+        #This initial condition sends a beam out the boresight.  Remember, the
+        #zeros are the phase angles of the unit power vectors at each AIP.
+        self.BeamDelays= np.zeros(self.gridshape);
+        self.elements = self.Ygrid.size;  #number of elements
+        
+    def AzRotate(self,az):
+        # provides the x and y positions of the array AIPs, in
+        # coordinate frame rotated to align with the Az direction
+        # First, allocate space for the answer: that's a class data element
+        # so, as it's done, we can immediately load it.
+        cosAz=np.cos(az);
+        sinAz=np.sin(az);
+        # Sense of this rotation is CCW, with x right & y up, on the page
+        # So the rotation is +z, out of the page, towards you.
+        # Python question: can I assume the "self." eg on self.Xgrid? A:no.
+        
+        #I need a view or something to index through with one iterator. In 
+        #the below, matrix.reshape does not remove a dimension.
+        Xarray=self.Xgrid.A1;      #Danger: no copy. ObjRef.
+        Yarray=self.Ygrid.A1;
+        for i in range(0, self.elements) :
+            self.AzYgrid.A1[i] = -Xarray[i]*sinAz + Yarray[i]*cosAz;
+            self.AzXgrid.A1[i] = Xarray[i]*cosAz + Yarray[i]*sinAz;
+   
+    def RampAdd(el):
+        #Caution: you better have performed AzRotate first!
+        #We are going to load the BeamDelays, as needed to create
+        #the squint angle el, in the az frame defined by AzGrid.
+        #(el is the "squint" angle away from aperture boresight)
+        
+        c= 3e8;  #m/sec, speed of light
+        
+        #how much addtl delay must we add for every meter in -x
+        delayRamp = sin(el)/c;  #something like this
+    
+        #First, make a temp linear array pointer into BeamDelays:
+        BeamDelayArray=self.BeamDelays.reshape(self.elements)
+        #Now go through the AIPs, adding delay proportionate to the x-coord
+        for i in range(0,self.elements):
+            #N.B. BeamDelayArray == BeamDelays so we are loading "both."
+            BeamDelayArray[i]= -delayRamp*AzYgrid[i];
+            #N.B. "-" because El is +x rotation: +y is farther so delay < 0.
+
+        
+    def aim(self,az,el):  #Loads the Delays for a commanded beam direction.
+        #Transform all the array AIP grid locations to the Az frame 
+        #Python Q: is self already local in AzRotate?
+        self.BeamAzAngle=az;  # make these part of the class state.
+        self.BeamElAngle=el;
+        self.AzRotate(az);  #x-form to az frame, where we can...
+        # apply the needed phase delays to all AIP to squint through el.
+        # (This function will set the BeamDelays)
+        self.RampAdd(el);
+        
+    def AzAngle(self,whichElement):
+        Xarray=self.Xgrid.reshape(self.elements); #Danger: no copy. ObjRef.
+        Yarray=self.Ygrid.reshape(self.elements);
+        angle = atan2(Yarray[i],Xarray[i]);
+        return(angle);
+                   
+
+#------------------------------------------------------------------
+
+
+# here's the Main.  Takes Az and El of the commanded beam direction, and 
+# arrays of the null direction commands.
+# N.B. This is not a class method.
+# N.B. fn definition syntax that puts defaults into the parameter list.
+
+beamFormExample();
     
     
 #==============================================================================
